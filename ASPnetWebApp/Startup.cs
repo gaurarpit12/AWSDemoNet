10ebcb8d8b6e1dfead72576ddabeeba5e256df5d
Arpit Gaur <arpitga@cybage.com>
2018-02-19 12:42:43 +0530

    new files added to new repo, first commit

---
+++ ASPnetWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPnetWebApp.Startup))]
+namespace ASPnetWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
